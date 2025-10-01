91bddb247e8d4240712b171127d5ed1f9fbc5c8a
tory <chkoreakr@google.com>
2025-10-01 22:22:46 +0900

    1001

--- src/B2/N_8958.java
+++ src/B2/N_8958.java
@@ -1,4 +1,15 @@
 package B2;
+/*
+import java.util.Scanner;
 
 public class N_8958 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for(int i = 0; i < T; i++){
+
+    }
+
+    }
 }
+*/
