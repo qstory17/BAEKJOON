c1be72eef3b4758ce4530c1eeaf2163654369dec
qstory17 <chkoreakr@gmail.com>
2025-10-06 23:41:10 +0900

    1006
    1006

---
+++ N_2744.java
@@ -0,0 +1,36 @@
+package B5;
+/*
+문제
+영어 소문자와 대문자로 이루어진 단어를 입력받은 뒤, 대문자는 소문자로, 소문자는 대문자로 바꾸어 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 영어 소문자와 대문자로만 이루어진 단어가 주어진다. 단어의 길이는 최대 100이다.
+
+출력
+첫째 줄에 입력으로 주어진 단어에서 대문자는 소문자로, 소문자는 대문자로 바꾼 단어를 출력한다.
+
+예제 입력 1
+WrongAnswer
+예제 출력 1
+wRONGaNSWER
+ */
+
+import java.util.Scanner;
+
+public class N_2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLowerCase(c)) {
+                c = Character.toUpperCase(c);
+
+            } else if (Character.isUpperCase(c))
+                c = Character.toLowerCase(c);
+
+            System.out.print(c);
+        }
+    }
+}
