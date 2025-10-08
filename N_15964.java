0ea876ba3af4b429f4f0257dda7c89e6c10d78ba
qstory17 <chkoreakr@gmail.com>
2025-10-08 22:51:05 +0900

    1008
    1008 개꿀문제.. 쉬어가자.. 근데.. 30점? 머지?

---
+++ N_15964.java
@@ -0,0 +1,15 @@
+package B5;
+//개꿀
+import java.util.Scanner;
+
+public class N_15964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A*A-B*B);
+
+        sc.close();
+    }
+}
