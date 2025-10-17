15938ca167b030d7da0207291f59ab447b5f7d43
qstory17 <chkoreakr@gmail.com>
2025-10-17 23:09:14 +0900

    1017
    1017
    최대공약수, 최소공배수
    while (b != 0) {
                int r = a % b;
                a = b;
                b = r;
            }
            return a;
    
            int gcd = gcd(A, B);
            int lcm = (A * B) / gcd;

---
+++ N_2609.java
@@ -0,0 +1,44 @@
+package B1;
+/*
+문제
+두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+
+출력
+첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+
+예제 입력 1
+24 18
+예제 출력 1
+6
+72
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class N_2609 {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int gcd = gcd(A, B);
+        int lcm = (A * B) / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+}
