20737a9b0127e015f970e74e3c1fd921c134db1a
qstory17 <chkoreakr@gmail.com>
2025-10-18 23:37:09 +0900

    1018
    1018 단순한 덧셈

---
+++ N_1000.java
@@ -0,0 +1,16 @@
+package B5;
+
+import java.util.Scanner;
+
+public class N_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+
+        sc.close();
+    }
+}
