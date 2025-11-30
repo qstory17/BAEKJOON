85fc984b95f98e35bd0c13daee3556be1b0d405a
qstory17 <chkoreakr@gmail.com>
2025-11-30 23:55:22 +0900

    1130
    1130

---
+++ B4_5565.java
@@ -0,0 +1,19 @@
+package B4;
+
+import java.util.Scanner;
+
+public class B4_5565 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = sc.nextInt();
+
+        for(int i=0; i<9; i++) {
+            sum -= sc.nextInt();
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
