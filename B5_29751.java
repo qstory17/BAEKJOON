6c9fa0f8bd6e7ee3d6912f049647b87bf19e5d56
qstory17 <chkoreakr@gmail.com>
2025-12-05 15:52:19 +0900

    1205
    1205

---
+++ B5_29751.java
@@ -0,0 +1,23 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B5_29751 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        double result = 0;
+
+        result = (W*H)/2.0;
+        System.out.printf("%.1f", result);
+    }
+}
