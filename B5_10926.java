babbcfdd9c381a3f0e5ea8ef475f4784ed9393ef
qstory17 <chkoreakr@gmail.com>
2025-12-25 02:04:19 +0900

    1225
    1225

---
+++ B5_10926.java
@@ -0,0 +1,15 @@
+package B5;
+
+import java.util.Scanner;
+
+public class B5_10926 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            String id = sc.nextLine();
+
+            System.out.println(id + "??!");
+
+            sc.close();
+        }
+    }
