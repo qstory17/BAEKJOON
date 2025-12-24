c60cadf9a89df7b7314413f0da5e3873425b2730
qstory17 <chkoreakr@gmail.com>
2025-12-24 22:55:24 +0900

    1224
    1224

---
+++ B5_15740.java
@@ -0,0 +1,18 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class B5_15740 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.println(A.add(B));
+    }
+}
