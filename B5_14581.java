0de36096f47f07cf3bbbfcff13e1b9d027d65a50
qstory17 <chkoreakr@gmail.com>
2025-12-27 05:57:53 +0900

    1227
    1227

---
+++ B5_14581.java
@@ -0,0 +1,17 @@
+package B5;
+
+import java.util.Scanner;
+
+public class B5_14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+        sc.close();
+    }
+}
